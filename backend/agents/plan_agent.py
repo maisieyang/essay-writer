cfbdad9a26c467675c95f2367bb0e9e2f155cb62
maisieyang <yangxiyuebupt@gmail.com>
2024-08-26 14:04:13 +0800

    fix: plan prompt
    fix: plan prompt

--- backend/agents/plan_agent.py
+++ backend/agents/plan_agent.py
@@ -4,8 +4,10 @@ from langchain_core.messages import SystemMessage, HumanMessage
 class PlanAgent:
     def __init__(self, model=None):
         self.model = model or ChatOpenAI(model="gpt-3.5-turbo", temperature=1)
-        self.PLAN_PROMPT = ("作为一名写作专家，请为一个三段式的简短文章撰写一份高级别的提纲。根据用户提供的主题，明确文章的三个主要部分，并为每个部分提供相应的说明或写作建议。请用中文回答。 ")
-    
+        self.PLAN_PROMPT = ("作为一名写作专家，请为一个三段式的简短文章撰写一份高级别的提纲。"
+                            "根据用户提供的主题，明确文章的三个主要部分，并为每个部分提供相应的说明或写作建议。"
+                            "请用中文回答。 "
+                            )
     def run(self, state):
         messages = [
             SystemMessage(content=self.PLAN_PROMPT), 
