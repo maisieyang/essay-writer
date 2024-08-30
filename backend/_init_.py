acf96f710550b5c344e0266ee26d56ec3e803cfa
maisieyang <yangxiyuebupt@gmail.com>
2024-08-30 17:43:06 +0800

    Create _init_.py

---
+++ backend/_init_.py
@@ -0,0 +1,20 @@
+# backend/__init__.py
+
+
+from .agents import (
+    PlanAgent,
+    ResearchAgent,
+    WriterAgent,
+    ReflectionAgent,
+    CritiqueAgent,
+    ManagerAgent
+)
+
+__all__ = [
+    PlanAgent,
+    ResearchAgent,
+    WriterAgent,
+    ReflectionAgent,
+    CritiqueAgent,
+    ManagerAgent
+]
